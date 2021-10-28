fc415d2f245a3b82245eb501f54bf32b7d281f26
Matias Sourigues <mjsourigues@gmail.com>
2021-10-28 11:51:00 -0300

    Updates

--- src/Axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL:"http://localhost:5001/challenge-21021/us-central1/api"  //API (CLOUD FUNCTION URL)
-});
-
-export default instance;
